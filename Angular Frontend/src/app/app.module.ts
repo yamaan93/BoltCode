a190f17a8b2fc27c994884edf786b92bd3ea1f49
Keyon-Jerome <keyonjerome@gmail.com>
2019-10-05 18:10:25 -0400

    Added job posting, removed boilerplate.

--- Angular Frontend/src/app/app.module.ts
+++ Angular Frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { JobPostingComponent } from './job-posting/job-posting.component';
 
 @NgModule({
   imports: [
@@ -26,7 +27,8 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    JobPostingComponent
 
   ],
   providers: [],
---
+++ Angular Frontend/src/app/job-posting/job-posting.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JobPostingComponent } from './job-posting.component';
+
+describe('JobPostingComponent', () => {
+  let component: JobPostingComponent;
+  let fixture: ComponentFixture<JobPostingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JobPostingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobPostingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
