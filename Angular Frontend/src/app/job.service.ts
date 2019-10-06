831f815f62756edb303ead71861ade45a554a3f9
Keyon-Jerome <keyonjerome@gmail.com>
2019-10-06 03:18:53 -0400

    Styling, job display changes.

--- Angular Frontend/src/app/job.service.ts
+++ Angular Frontend/src/app/job.service.ts
@@ -7,7 +7,7 @@ export class JobService {
   public jobPostings: { title:string,description:string,companyName:string,date:Date,languages: string,imageURL: string, poster:string, reward:number}[];
   constructor() { 
     this.jobPostings = [];
-    for (let i in [1,2,3,4,5,6,7,8]) {
+    for (let i in [1,2,3,4,5,6,7,8,9,10]) {
     this.jobPostings.push({title:"Angular Frontend Development",
                       description:("Add a generic sidebar to RBC's in-development careers page. Figma UI/UX will be provided and must be replicated. Mobile view MUST be taken into consideration."),
                       date:new Date(),languages:"Angular 8, HTML, CSS, TypeScript",
